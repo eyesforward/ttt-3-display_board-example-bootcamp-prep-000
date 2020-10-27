0e4be7e2b1441ada8037946f0b6bd5020d2e81bd
dapper-firmware-5532 <eyesforwardfilms@gmail.com>
2020-10-27 22:32:06 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,12 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+ var display_board= "Tic Tac Toe"
+ 
+ TIc Tac Toe = [" ", " ", " ", " ", " ", " ", " ", "
+  ", " "]
+
+def Tic Tac Toe
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
